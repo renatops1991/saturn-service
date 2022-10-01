12289d84e07ff5071e95750f76db6602b1664000
Renato Pereira <renatops1991@gmail.com>
2022-10-01 00:28:57 -0300

    refactor: make SignUpUserController follow to a Controller protocol

--- src/presentation/controllers/signup-user-controller.ts
+++ src/presentation/controllers/signup-user-controller.ts
@@ -1,10 +1,11 @@
 import { UserDto } from '../dtos/user.dto'
 import { MissingMandatoryParamError } from '../errors/missing-mandatory-param-error'
 import { badRequest } from '../http-helper'
+import { Controller } from '../protocols/controller'
 import { HttpResponse } from '../protocols/http'
 
-export class UserController {
-  create (userDto: UserDto): HttpResponse {
+export class SignUpUserController implements Controller {
+  handle (userDto: UserDto): HttpResponse {
     if (!userDto.name) {
       return badRequest(new MissingMandatoryParamError('name'))
     }
--- tests/unit/presentation/controllers/user-controller.spec.ts
+++ tests/unit/presentation/controllers/user-controller.spec.ts
@@ -1,55 +1,55 @@
-import { UserController } from '../../../../src/presentation/controllers/user-controller'
+import { SignUpUserController } from '../../../../src/presentation/controllers/signup-user-controller'
 import { MissingMandatoryParamError } from '../../../../src/presentation/errors/missing-mandatory-param-error'
 
 describe('User Controller', () => {
   it('Should return 400 if no name is provided', async () => {
-    const sut = new UserController()
+    const sut = new SignUpUserController()
     const userDto = {
       name: '',
       email: 'foo@example.com',
       password: '12345',
       passwordConfirmation: '12345'
     }
-    const expectedResponse = sut.create(userDto)
+    const expectedResponse = sut.handle(userDto)
     expect(expectedResponse.statusCode).toBe(400)
     expect(expectedResponse.body).toEqual(new MissingMandatoryParamError('name'))
   })
 
   it('Should return 400 if no email is provided', async () => {
-    const sut = new UserController()
+    const sut = new SignUpUserController()
     const userDto = {
       name: 'John Foo Bar',
       email: '',
       password: '12345',
       passwordConfirmation: '12345'
     }
-    const expectedResponse = sut.create(userDto)
+    const expectedResponse = sut.handle(userDto)
     expect(expectedResponse.statusCode).toBe(400)
     expect(expectedResponse.body).toEqual(new MissingMandatoryParamError('email'))
   })
 
   it('Should return 400 if no password is provided', async () => {
-    const sut = new UserController()
+    const sut = new SignUpUserController()
     const userDto = {
       name: 'John Foo Bar',
       email: 'foo@example.com',
       password: '',
       passwordConfirmation: '12345'
     }
-    const expectedResponse = sut.create(userDto)
+    const expectedResponse = sut.handle(userDto)
     expect(expectedResponse.statusCode).toBe(400)
     expect(expectedResponse.body).toEqual(new MissingMandatoryParamError('password'))
   })
 
   it('Should return 400 if no passwordConfirmation is provided', async () => {
-    const sut = new UserController()
+    const sut = new SignUpUserController()
     const userDto = {
       name: 'John Foo Bar',
       email: 'foo@example.com',
       password: '12345',
       passwordConfirmation: ''
     }
-    const expectedResponse = sut.create(userDto)
+    const expectedResponse = sut.handle(userDto)
     expect(expectedResponse.statusCode).toBe(400)
     expect(expectedResponse.body).toEqual(new MissingMandatoryParamError('passwordConfirmation'))
   })
