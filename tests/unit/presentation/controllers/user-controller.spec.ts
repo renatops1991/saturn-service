0688b943c5c71aba594baf16709f6c852fef4b39
Renato Pereira <renatops1991@gmail.com>
2022-10-01 12:53:42 -0300

    test: ensure calls isValid method to validate email

--- tests/unit/presentation/controllers/user-controller.spec.ts
+++ tests/unit/presentation/controllers/user-controller.spec.ts
@@ -1,16 +1,30 @@
 import { SignUpUserController } from '../../../../src/presentation/controllers/signup-user-controller'
 import { MissingMandatoryParamError } from '../../../../src/presentation/errors/missing-mandatory-param-error'
+import { EmailValidator } from '../../../../src/presentation/protocols/email-validator'
 
 interface SutTypes {
   sut: SignUpUserController
+  emailValidatorStub: EmailValidator
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new SignUpUserController()
+  const emailValidatorStub = mockEmailValidator()
+  const sut = new SignUpUserController(emailValidatorStub)
 
   return {
-    sut
+    sut,
+    emailValidatorStub
   }
+}
+
+const mockEmailValidator = (): EmailValidator => {
+  class EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidator()
 }
 
 describe('User Controller', () => {
@@ -65,5 +79,18 @@ describe('User Controller', () => {
     const expectedResponse = sut.handle(userDto)
     expect(expectedResponse.statusCode).toBe(400)
     expect(expectedResponse.body).toEqual(new MissingMandatoryParamError('passwordConfirmation'))
+  })
+
+  it('Should call is valid method with correct value', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const userDto = {
+      name: 'John Foo Bar',
+      email: 'foo@example.com',
+      password: '12345',
+      passwordConfirmation: '12345'
+    }
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    sut.handle(userDto)
+    expect(isValidSpy).toHaveBeenCalledWith(userDto.email)
   })
 })
