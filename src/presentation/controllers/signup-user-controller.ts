79e4248dae8bdcce8c66c3e2b367c70f370b6753
Renato Pereira <renatops1991@gmail.com>
2022-10-01 12:52:56 -0300

    feat: ensure calls isValid method to validate email

--- src/presentation/controllers/signup-user-controller.ts
+++ src/presentation/controllers/signup-user-controller.ts
@@ -2,8 +2,13 @@ import { UserDto } from '../dtos/user.dto'
 import { MissingMandatoryParamError } from '../errors/missing-mandatory-param-error'
 import { badRequest } from '../http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 import { HttpResponse } from '../protocols/http'
 export class SignUpUserController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   handle (userDto: UserDto): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -11,5 +16,7 @@ export class SignUpUserController implements Controller {
         return badRequest(new MissingMandatoryParamError(field))
       }
     }
+
+    this.emailValidator.isValid(userDto.email)
   }
 }
