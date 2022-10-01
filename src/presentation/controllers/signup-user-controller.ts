0c5df3bd02a18a8d172ffc91181316de9badf923
Renato Pereira <renatops1991@gmail.com>
2022-10-01 13:33:21 -0300

    feat: ensure that SignUpController throw exception error if return 500 error

--- src/presentation/controllers/signup-user-controller.ts
+++ src/presentation/controllers/signup-user-controller.ts
@@ -1,6 +1,7 @@
 import { UserDto } from '../dtos/user.dto'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingMandatoryParamError } from '../errors/missing-mandatory-param-error'
+import { ServerError } from '../errors/sever-error'
 import { badRequest } from '../http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
@@ -11,16 +12,23 @@ export class SignUpUserController implements Controller {
   ) {}
 
   handle (userDto: UserDto): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!userDto[field]) {
-        return badRequest(new MissingMandatoryParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!userDto[field]) {
+          return badRequest(new MissingMandatoryParamError(field))
+        }
       }
-    }
 
-    const isValidEmail = this.emailValidator.isValid(userDto.email)
-    if (!isValidEmail) {
-      return badRequest(new InvalidParamError('email'))
+      const isValidEmail = this.emailValidator.isValid(userDto.email)
+      if (!isValidEmail) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
