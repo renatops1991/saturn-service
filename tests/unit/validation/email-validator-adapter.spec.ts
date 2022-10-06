1b3d7b29c3e48cb6f802c94421d83a435b787099
Renato Pereira <renatops1991@gmail.com>
2022-10-05 23:36:59 -0300

    refactor: move EmailValidatorAdapter class to sut

--- tests/unit/validation/email-validator-adapter.spec.ts
+++ tests/unit/validation/email-validator-adapter.spec.ts
@@ -6,22 +6,33 @@ jest.mock('validator', () => ({
     return true
   }
 }))
+
+interface SutType {
+  sut: EmailValidatorAdapter
+}
+
+const makeSut = (): SutType => {
+  const sut = new EmailValidatorAdapter()
+  return {
+    sut
+  }
+}
 describe('EmailValidatorAdapter', () => {
   it('Should return false if email passed is invalid ', async () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('foo')
     expect(isValid).toBe(false)
   })
 
   it('Should return true if email passed is valid ', async () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     const isValid = sut.isValid('foo@email.com')
     expect(isValid).toBe(true)
   })
 
   it('Should call validator with correct value ', async () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     const email = 'foo@email.com'
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid(email)
