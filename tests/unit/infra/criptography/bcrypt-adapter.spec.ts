10a6b55d12a133ddb5500cd9db97fb27fb149a9b
Renato Pereira <renatops1991@gmail.com>
2022-10-11 00:12:02 -0300

    test: ensure BcrypAdapter returns a correct hash on success

--- tests/unit/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/unit/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,11 +2,10 @@
 import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
+const salt = 12
 type SutType = {
   sut: BcryptAdapter
 }
-
-const salt = 12
 
 const makeSut = (): SutType => {
   const sut = new BcryptAdapter(salt)
@@ -15,11 +14,23 @@ const makeSut = (): SutType => {
   }
 }
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hashPassword'))
+  }
+}))
+
 describe('BcryptAdapter', () => {
   it('Should call bcrypt with correct value', async () => {
     const { sut } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.encrypt('hashPassword')
-    expect(hashSpy).toHaveBeenCalledWith('hashPassword', salt)
+    await sut.encrypt('password')
+    expect(hashSpy).toHaveBeenCalledWith('password', salt)
+  })
+
+  it('Should return a correct has on success', async () => {
+    const { sut } = makeSut()
+    const expectedResponse = await sut.encrypt('password')
+    expect(expectedResponse).toEqual('hashPassword')
   })
 })
