50c1934f8546868cd6cb5e5c266596452d3ea3cc
Renato Pereira <renatops1991@gmail.com>
2022-10-10 23:25:42 -0300

    refactor: move entities to domain layer

--- src/data/builders/user-builder.ts
+++ src/data/builders/user-builder.ts
@@ -1,7 +1,7 @@
 import { CreateUserDto } from '@/presentation/dtos/user/create-user.dto'
 import { UserBuilderInterface } from '@/data/protocols/user-builder'
 import { UserBasicInfo } from '@/types/user-basic-info'
-import { User } from '@/entities/user'
+import { User } from '@/domain/entities/user'
 
 export class UserBuilder implements UserBuilderInterface {
   buildUserBasicInfo (userDto: CreateUserDto): UserBasicInfo {
