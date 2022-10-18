608335972484a06c00479ea89bd0256baacc01e4
Renato Pereira <renatops1991@gmail.com>
2022-10-18 00:26:44 -0300

    refactor: change require to import for import config app

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 MongoConnect.connect(process.env.MONGODB_URI)
   .then(async () => {
-    const { setupApp } = await require('./config/app')
+    const { setupApp } = await import ('./config/app')
     const app = await setupApp()
     app.listen(process.env.PORT, () => console.log(String.fromCodePoint(0x1F525) + ' Server is running on port ' + process.env.PORT))
   })
