6526c514ce75cdb61f0e5eeab5907a70a1588c4e
Renato Pereira <renatops1991@gmail.com>
2022-11-01 08:54:56 -0300

    test: ensure return 400 if email no provided

---
+++ tests/unit/presentation/controllers/user/login-user-controller.spec.ts
@@ -0,0 +1,16 @@
+import { LoginUserController } from '@/presentation/controllers/user/login-user-controller'
+import { LoginUserDto } from '@/presentation/dtos/user/login-user.dto'
+import { MissingMandatoryParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/http-helper'
+
+describe('LoginUserController', () => {
+  it('Should return 400 error if email no provided', async () => {
+    const sut = new LoginUserController()
+    const loginUserDto: LoginUserDto = {
+      email: '',
+      password: 'foo'
+    }
+    const expectedResponse = await sut.handle(loginUserDto)
+    expect(expectedResponse).toEqual(badRequest(new MissingMandatoryParamError('email').serializeErrors()))
+  })
+})
