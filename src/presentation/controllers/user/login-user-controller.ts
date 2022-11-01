44cb2b2ed1a6dd5c69c0c2d7696f7f1deeb1073b
Renato Pereira <renatops1991@gmail.com>
2022-11-01 09:02:53 -0300

    feat: ensure call isValid method with correct value

--- src/presentation/controllers/user/login-user-controller.ts
+++ src/presentation/controllers/user/login-user-controller.ts
@@ -2,9 +2,11 @@ import { LoginUserDto } from '@/presentation/dtos/user/login-user.dto'
 import { MissingMandatoryParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/http-helper'
 import { IController } from '@/presentation/protocols/controller'
+import { IEmailValidator } from '@/presentation/protocols/email-validator'
 import { IHttpResponse } from '@/presentation/protocols/http'
 
 export class LoginUserController implements IController {
+  constructor (private readonly emailValidator: IEmailValidator) {}
   async handle (loginUserDto: LoginUserDto): Promise<IHttpResponse> {
     const requiredFields = ['email', 'password']
     for (const field of requiredFields) {
@@ -12,5 +14,7 @@ export class LoginUserController implements IController {
         return await new Promise(resolve => resolve(badRequest(new MissingMandatoryParamError(field).serializeErrors())))
       }
     }
+
+    this.emailValidator.isValid(loginUserDto.email)
   }
 }
--- tests/unit/presentation/controllers/user/login-user-controller.spec.ts
+++ tests/unit/presentation/controllers/user/login-user-controller.spec.ts
@@ -2,10 +2,25 @@ import { LoginUserController } from '@/presentation/controllers/user/login-user-
 import { LoginUserDto } from '@/presentation/dtos/user/login-user.dto'
 import { MissingMandatoryParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/http-helper'
+import { IEmailValidator } from '@/presentation/protocols/email-validator'
+import { mockEmailValidator } from '../../mocks/mock-email-validator'
 
+type sutTypes = {
+  sut: LoginUserController
+  emailValidatorStub: IEmailValidator
+}
+
+const makeSut = (): sutTypes => {
+  const emailValidatorStub = mockEmailValidator()
+  const sut = new LoginUserController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
 describe('LoginUserController', () => {
   it('Should return 400 error if email no provided', async () => {
-    const sut = new LoginUserController()
+    const { sut } = makeSut()
     const loginUserDto: LoginUserDto = {
       email: '',
       password: 'foo'
@@ -15,12 +30,23 @@ describe('LoginUserController', () => {
   })
 
   it('Should return 400 error if password no provided', async () => {
-    const sut = new LoginUserController()
+    const { sut } = makeSut()
     const loginUserDto: LoginUserDto = {
       email: 'foo@example.com',
       password: ''
     }
     const expectedResponse = await sut.handle(loginUserDto)
     expect(expectedResponse).toEqual(badRequest(new MissingMandatoryParamError('password').serializeErrors()))
+  })
+
+  it('Should call isValid method of the EmailValidator class with correct value', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const loginUserDto: LoginUserDto = {
+      email: 'foo@example.com',
+      password: 'foo'
+    }
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    await sut.handle(loginUserDto)
+    expect(isValidSpy).toHaveBeenCalledWith(loginUserDto.email)
   })
 })
