5d46f7f3be13aad56c54cdeb258644ddf58cec6b
Renato Pereira <renatops1991@gmail.com>
2022-11-01 23:37:29 -0300

    feat: ensure LoginUserController return 500 error throws exception error

--- src/presentation/controllers/user/login-user-controller.ts
+++ src/presentation/controllers/user/login-user-controller.ts
@@ -1,6 +1,6 @@
 import { LoginUserDto } from '@/presentation/dtos/user/login-user.dto'
 import { InvalidParamError, MissingMandatoryParamError } from '@/presentation/errors'
-import { badRequest } from '@/presentation/http-helper'
+import { badRequest, serverError } from '@/presentation/http-helper'
 import { IController } from '@/presentation/protocols/controller'
 import { IEmailValidator } from '@/presentation/protocols/email-validator'
 import { IHttpResponse } from '@/presentation/protocols/http'
@@ -8,17 +8,21 @@ import { IHttpResponse } from '@/presentation/protocols/http'
 export class LoginUserController implements IController {
   constructor (private readonly emailValidator: IEmailValidator) { }
   async handle (loginUserDto: LoginUserDto): Promise<IHttpResponse> {
-    const requiredFields = ['email', 'password']
-    for (const field of requiredFields) {
-      if (!loginUserDto[field]) {
-        return await new Promise(resolve => resolve(badRequest(new MissingMandatoryParamError(field).serializeErrors())))
+    try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!loginUserDto[field]) {
+          return await new Promise(resolve => resolve(badRequest(new MissingMandatoryParamError(field).serializeErrors())))
+        }
       }
-    }
 
-    const { email } = loginUserDto
-    const isValidEmail = this.emailValidator.isValid(email)
-    if (!isValidEmail) {
-      return await new Promise(resolve => resolve(badRequest(new InvalidParamError('email').serializeErrors())))
+      const { email } = loginUserDto
+      const isValidEmail = this.emailValidator.isValid(email)
+      if (!isValidEmail) {
+        return await new Promise(resolve => resolve(badRequest(new InvalidParamError('email').serializeErrors())))
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
--- tests/unit/presentation/controllers/user/login-user-controller.spec.ts
+++ tests/unit/presentation/controllers/user/login-user-controller.spec.ts
@@ -1,6 +1,6 @@
 import { LoginUserController } from '@/presentation/controllers/user/login-user-controller'
-import { InvalidParamError, MissingMandatoryParamError } from '@/presentation/errors'
-import { badRequest } from '@/presentation/http-helper'
+import { InvalidParamError, MissingMandatoryParamError, ServerError } from '@/presentation/errors'
+import { badRequest, serverError } from '@/presentation/http-helper'
 import { IEmailValidator } from '@/presentation/protocols/email-validator'
 import { fixturesLoginUser } from '../../fixtures/fixtures-user'
 import { mockEmailValidator } from '../../mocks/mock-email-validator'
@@ -39,15 +39,23 @@ describe('LoginUserController', () => {
     const { sut, emailValidatorStub } = makeSut()
     const loginUserDto = fixturesLoginUser()
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
-    await sut.handle(loginUserDto)
+    await sut.handle(fixturesLoginUser())
     expect(isValidSpy).toHaveBeenCalledWith(loginUserDto.email)
   })
 
   it('Should return 400 error if email is invalid', async () => {
     const { sut, emailValidatorStub } = makeSut()
-    const loginUserDto = fixturesLoginUser()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const expectedResponse = await sut.handle(loginUserDto)
+    const expectedResponse = await sut.handle(fixturesLoginUser())
     expect(expectedResponse).toEqual(badRequest(new InvalidParamError('email').serializeErrors()))
+  })
+
+  it('Should return 500 error if LoginUserController throws exception error', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const expectedResponse = await sut.handle(fixturesLoginUser())
+    expect(expectedResponse).toEqual(serverError(new ServerError(expectedResponse.body.stack)))
   })
 })
