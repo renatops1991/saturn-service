a97a05082f658befa04a312749ddad5ae91a938d
Renato Santos <renatops1991@gmail.com>
2022-11-16 00:55:29 -0300

    test: ensure call compare method with correct value

--- tests/unit/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/unit/infra/criptography/bcrypt-adapter.spec.ts
@@ -17,27 +17,42 @@ const makeSut = (): SutType => {
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return await new Promise(resolve => resolve('hashPassword'))
+  },
+
+  async compare (): Promise<boolean> {
+    return await new Promise(resolve => resolve(true))
   }
 }))
 
 describe('BcryptAdapter', () => {
-  it('Should call bcrypt with correct value', async () => {
-    const { sut } = makeSut()
-    const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.hash('password')
-    expect(hashSpy).toHaveBeenCalledWith('password', salt)
+  describe('hash', () => {
+    it('Should call hash method with correct value', async () => {
+      const { sut } = makeSut()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.hash('password')
+      expect(hashSpy).toHaveBeenCalledWith('password', salt)
+    })
+
+    it('Should return a correct hash on success', async () => {
+      const { sut } = makeSut()
+      const expectedResponse = await sut.hash('password')
+      expect(expectedResponse).toEqual('hashPassword')
+    })
+
+    it('Should pass error if hash method throws exception error', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+      const expectedResponse = sut.hash('password')
+      await expect(expectedResponse).rejects.toThrow()
+    })
   })
 
-  it('Should return a correct has on success', async () => {
-    const { sut } = makeSut()
-    const expectedResponse = await sut.hash('password')
-    expect(expectedResponse).toEqual('hashPassword')
-  })
-
-  it('Should pass error if bcrypt method throws exception error', async () => {
-    const { sut } = makeSut()
-    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
-    const expectedResponse = sut.hash('password')
-    await expect(expectedResponse).rejects.toThrow()
+  describe('compare', () => {
+    it('Should call compare method with correct value', async () => {
+      const { sut } = makeSut()
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await sut.compare('password', 'hashPassword')
+      expect(compareSpy).toHaveBeenCalledWith('password', 'hashPassword')
+    })
   })
 })
