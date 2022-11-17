fc49996e3b18521f089f2e9dcc4a6444a1c2cc52
Renato Pereira <renatops1991@gmail.com>
2022-11-16 23:45:06 -0300

    test: ensure pass error if sign method throws exception error

--- tests/unit/infra/criptography/jwt-adapter.spec.ts
+++ tests/unit/infra/criptography/jwt-adapter.spec.ts
@@ -7,17 +7,30 @@ jest.mock('jsonwebtoken', () => ({
   }
 }))
 
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('foo')
+}
+
 describe('JwtAdapter', () => {
   it('Should call sign method with correct value', async () => {
-    const sut = new JwtAdapter('foo')
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('bar')
     expect(signSpy).toHaveBeenLastCalledWith({ id: 'bar' }, 'foo')
   })
 
   it('Should return a token if sign method on succeeds', async () => {
-    const sut = new JwtAdapter('foo')
+    const sut = makeSut()
     const expectedResponse = await sut.encrypt('bar')
     expect(expectedResponse).toBe('token')
+  })
+
+  it('Should pass error if sign method throws exception error', async () => {
+    const sut = makeSut()
+    jest
+      .spyOn(jwt, 'sign')
+      .mockImplementationOnce(() => { throw new Error() })
+    const expectedResponse = sut.encrypt('bar')
+    await expect(expectedResponse).rejects.toThrow()
   })
 })
