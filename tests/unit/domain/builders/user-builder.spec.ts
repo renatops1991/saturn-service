a2e54081cc29941f5a63b6988e0f0d6754185aba
Renato Pereira <renatops1991@gmail.com>
2022-11-21 23:44:55 -0300

    test: ensure build user with confirmUser field equal false if not provided

--- tests/unit/domain/builders/user-builder.spec.ts
+++ tests/unit/domain/builders/user-builder.spec.ts
@@ -2,9 +2,22 @@ import { UserBuilder } from '@/data/builders/user-builder'
 import { fixturesCreateUser } from '@/tests/unit/presentation/fixtures/fixtures-user'
 
 describe('UserBuilder', () => {
-  it('Should build user with correct', async () => {
+  it('Should build user with corrects values', async () => {
     const sut = new UserBuilder()
     const expectedResponse = sut.buildUserBasicInfo(fixturesCreateUser())
+    expect(expectedResponse).toEqual({
+      name: 'John Foo Bar',
+      email: 'foo@example.com',
+      password: '12345',
+      confirmUser: false
+    })
+  })
+
+  it('Should build user with confirmUser field equal false if not provided', async () => {
+    const sut = new UserBuilder()
+    const buildUser = fixturesCreateUser()
+    delete buildUser.confirmUser
+    const expectedResponse = sut.buildUserBasicInfo(buildUser)
     expect(expectedResponse).toEqual({
       name: 'John Foo Bar',
       email: 'foo@example.com',
