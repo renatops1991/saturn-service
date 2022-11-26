3d865c1b4c7eec1905d05b3fb9a9589f62bc09a4
Renato Santos <renatops1991@gmail.com>
2022-11-26 15:08:19 -0300

    test: ensure return a value if verify method on succeeds

--- tests/unit/infra/cryptography/jwt-adapter.spec.ts
+++ tests/unit/infra/cryptography/jwt-adapter.spec.ts
@@ -4,6 +4,9 @@ import jwt from 'jsonwebtoken'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return await new Promise(resolve => resolve('token'))
+  },
+  async verify (): Promise<string> {
+    return await new Promise(resolve => resolve('token'))
   }
 }))
 
@@ -12,25 +15,54 @@ const makeSut = (): JwtAdapter => {
 }
 
 describe('JwtAdapter', () => {
-  it('Should call sign method with correct value', async () => {
-    const sut = makeSut()
-    const signSpy = jest.spyOn(jwt, 'sign')
-    await sut.encrypt('bar')
-    expect(signSpy).toHaveBeenLastCalledWith({ id: 'bar' }, 'foo')
+  describe('encrypt', () => {
+    it('Should call sign method with correct value', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('bar')
+      expect(signSpy).toHaveBeenLastCalledWith({ id: 'bar' }, 'foo')
+    })
+
+    it('Should return a token if sign method on succeeds', async () => {
+      const sut = makeSut()
+      const expectedResponse = await sut.encrypt('bar')
+      expect(expectedResponse).toBe('token')
+    })
+
+    it('Should pass error if sign method throws exception error', async () => {
+      const sut = makeSut()
+      jest
+        .spyOn(jwt, 'sign')
+        .mockImplementationOnce(() => { throw new Error() })
+      const expectedResponse = sut.encrypt('bar')
+      await expect(expectedResponse).rejects.toThrow()
+    })
   })
 
-  it('Should return a token if sign method on succeeds', async () => {
-    const sut = makeSut()
-    const expectedResponse = await sut.encrypt('bar')
-    expect(expectedResponse).toBe('token')
-  })
+  describe('decrypt', () => {
+    it('Should call verify method of the JWT with correct value', async () => {
+      const sut = makeSut()
+      const verifySpy = jest
+        .spyOn(jwt, 'verify')
+      await sut.decrypt('accessToken')
+      expect(verifySpy).toHaveBeenCalledWith('accessToken', 'foo')
+    })
 
-  it('Should pass error if sign method throws exception error', async () => {
-    const sut = makeSut()
-    jest
-      .spyOn(jwt, 'sign')
-      .mockImplementationOnce(() => { throw new Error() })
-    const expectedResponse = sut.encrypt('bar')
-    await expect(expectedResponse).rejects.toThrow()
+    it('Should return a value if verify method on succeeds', async () => {
+      const sut = makeSut()
+      jest
+        .spyOn(jwt, 'verify')
+      const expectedResponse = await sut.decrypt('accessToken')
+      expect(expectedResponse).toEqual('token')
+    })
+
+    it('Should forward the error if verify method throws exception error', async () => {
+      const sut = makeSut()
+      jest
+        .spyOn(jwt, 'verify')
+        .mockImplementationOnce(() => { throw new Error() })
+      const expectedResponse = sut.decrypt('accessToken')
+      await expect(expectedResponse).rejects.toThrow()
+    })
   })
 })
