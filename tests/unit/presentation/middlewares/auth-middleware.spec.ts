b72de9ff0f5f69177045b686e8209df5f98d9482
Renato Pereira <renatops1991@gmail.com>
2022-11-26 03:59:19 -0300

    test: ensure auth middleware verify if accessToken is valid and return an user with accoundId

---
+++ tests/unit/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,62 @@
+import { IAuthentication } from '@/domain/protocols/authentication'
+import { AccessDeniedError } from '@/presentation/errors/access-denied-error'
+import { forbidden, serverError, success } from '@/presentation/http-helper'
+import { AuthMiddleware } from '@/presentation/middlewares/auth-middleware'
+import { mockAuthentication } from '../mocks/mock-authentication'
+
+type SutTypes = {
+  sut: AuthMiddleware
+  authenticationStub: IAuthentication
+}
+
+const makeSut = (role?: string): SutTypes => {
+  const authenticationStub = mockAuthentication()
+  const sut = new AuthMiddleware(authenticationStub, role)
+  return {
+    sut,
+    authenticationStub
+  }
+}
+
+describe('Auth Middleware', () => {
+  it('Should return 403 if x-access-token no exists in headers', async () => {
+    const { sut } = makeSut()
+    const expectedResponse = await sut.handle({})
+    expect(expectedResponse).toEqual(forbidden(new AccessDeniedError().serializeErrors()))
+  })
+
+  it('Should call loadUserByToken method with correct accessToken', async () => {
+    const role = 'admin'
+    const { sut, authenticationStub } = makeSut(role)
+    const loadUserByTokenSpy = jest
+      .spyOn(authenticationStub, 'loadUserByToken')
+    await sut.handle({
+      accessToken: 'accessToken'
+    })
+    expect(loadUserByTokenSpy).toHaveBeenCalledWith('accessToken', role)
+  })
+
+  it('Should return 403 error if  loadUserByToken returns null', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest
+      .spyOn(authenticationStub, 'loadUserByToken')
+      .mockResolvedValueOnce(null)
+    const expectedResponse = await sut.handle({ accessToken: 'accessToken' })
+    expect(expectedResponse).toEqual(forbidden(new AccessDeniedError().serializeErrors()))
+  })
+
+  it('Should return 500 error if loadUserByToken throws exception error', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest
+      .spyOn(authenticationStub, 'loadUserByToken')
+      .mockRejectedValue(new Error())
+    const expectedResponse = await sut.handle({ accessToken: 'accessToken' })
+    expect(expectedResponse).toEqual(serverError(new Error()))
+  })
+
+  it('Should return 200 if loadUserByToken method return an user', async () => {
+    const { sut } = makeSut()
+    const expectedResponse = await sut.handle({ accessToken: 'accessToken' })
+    expect(expectedResponse).toEqual(success({ accountId: 'foo' }))
+  })
+})
