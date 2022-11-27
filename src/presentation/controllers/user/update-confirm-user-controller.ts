51746e93070d0c2b378cd76b8d9f4ea8f95512d5
Renato Santos <renatops1991@gmail.com>
2022-11-27 16:05:02 -0300

    feat: ensure return 204 if update confirm user on succeeds

--- src/presentation/controllers/user/update-confirm-user-controller.ts
+++ src/presentation/controllers/user/update-confirm-user-controller.ts
@@ -1,6 +1,6 @@
 import { IUser } from '@/domain/protocols/user'
 import { UpdateConfirmUserDto } from '@/main/dtos/user'
-import { badRequest } from '@/presentation/http-helper'
+import { badRequest, noContent } from '@/presentation/http-helper'
 import { IController } from '@/presentation/protocols/controller'
 import { IHttpResponse } from '@/presentation/protocols/http'
 import { IValidation } from '@/presentation/protocols/validation'
@@ -18,5 +18,7 @@ export class UpdateConfirmUserController implements IController {
     }
 
     await this.user.updateConfirmUser(updateConfirmUser)
+
+    return noContent()
   }
 }
