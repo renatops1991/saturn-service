3578fa4c4c4d85bd2838f36f7a28f23b6eb5eb9c
Renato Pereira <renatops1991@gmail.com>
2022-12-16 08:04:44 -0300

    test: allow you to pass the field to be removed by parameter

--- tests/unit/main/utils.spec.ts
+++ tests/unit/main/utils.spec.ts
@@ -17,5 +17,20 @@ describe('Utils', () => {
         age: 31
       })
     })
+
+    it('Should return correctly only fields with values and remove field is provided by parameter', async () => {
+      const user = {
+        userId: 'foo',
+        name: 'bar',
+        email: '',
+        birthDate: null,
+        age: 31
+      }
+      const expectedResponse = utils.getFieldsWithValidValues(user, 'userId')
+      expect(expectedResponse).toEqual({
+        name: 'bar',
+        age: 31
+      })
+    })
   })
 })
