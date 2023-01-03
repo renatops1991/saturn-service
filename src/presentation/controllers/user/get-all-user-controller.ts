c9ec5bcefc4bc25376d220d5b48106e3e7c3586c
Renato Pereira <renatops1991@gmail.com>
2023-01-03 07:53:43 -0300

    feat: ensure return 500 error if getAllUsers method throws exception error

--- src/presentation/controllers/user/get-all-user-controller.ts
+++ src/presentation/controllers/user/get-all-user-controller.ts
@@ -1,6 +1,6 @@
 import { IUser } from '@/domain/protocols/user'
 import { GetUserDto } from '@/main/dtos/user'
-import { success } from '@/presentation/http-helper'
+import { serverError, success } from '@/presentation/http-helper'
 import { IController } from '@/presentation/protocols/controller'
 import { IHttpResponse } from '@/presentation/protocols/http'
 
@@ -8,7 +8,11 @@ export class GetAllUserController implements IController {
   constructor (private readonly user: IUser) {}
 
   async handle (getUserDto: GetUserDto): Promise<IHttpResponse> {
-    const users = await this.user.getAllUsers(getUserDto)
-    return success(users)
+    try {
+      const users = await this.user.getAllUsers(getUserDto)
+      return success(users)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
