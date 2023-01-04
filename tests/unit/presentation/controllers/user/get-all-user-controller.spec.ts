9b5d2a53866e3a207e5dfffc72f4ac3b70d1ae3e
Renato Pereira <renatops1991@gmail.com>
2023-01-04 07:57:13 -0300

    test: ensure call validate method to params validations provided

--- tests/unit/presentation/controllers/user/get-all-user-controller.spec.ts
+++ tests/unit/presentation/controllers/user/get-all-user-controller.spec.ts
@@ -1,23 +1,31 @@
 import { IUser } from '@/domain/protocols/user'
 import { GetUserDto } from '@/main/dtos/user'
 import { GetAllUserController } from '@/presentation/controllers/user/get-all-user-controller'
+import { badRequest, serverError, success } from '@/presentation/http-helper'
+import { IValidation } from '@/presentation/protocols/validation'
+import {
+  InvalidParamError,
+  ServerError
+} from '@/presentation/errors'
+import { mockValidation } from '@/tests/unit/presentation/mocks/mocks-user-validation'
 import { mocksUserController } from '@/tests/unit/presentation/mocks/mocks-user-controller'
 import { fixtureUpdateUserOutput } from '@/tests/unit/presentation/fixtures/fixtures-user'
-import { serverError, success } from '@/presentation/http-helper'
 import MockDate from 'mockdate'
-import { ServerError } from '@/presentation/errors'
 
 type SutTypes = {
   sut: GetAllUserController
+  validationStub: IValidation
   userStub: IUser
 }
 const makeSut = (): SutTypes => {
   const userStub = mocksUserController()
-  const sut = new GetAllUserController(userStub)
+  const validationStub = mockValidation()
+  const sut = new GetAllUserController(userStub, validationStub)
 
   return {
     sut,
-    userStub
+    userStub,
+    validationStub
   }
 }
 
@@ -54,6 +62,37 @@ describe('getAllUserController', () => {
     const users = await sut.handle(getUserDto)
     expect(users.statusCode).toEqual(200)
     expect(users).toEqual(success([fixtureUpdateUserOutput()]))
+  })
+
+  it('Should call validate method with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const getUserDto: GetUserDto = {
+      userId: 'foo',
+      document: '11111111',
+      startDate: new Date(),
+      endDate: new Date()
+    }
+    const validateSpy = jest
+      .spyOn(validationStub, 'validate')
+    await sut.handle(getUserDto)
+    expect(validateSpy).toHaveBeenCalledWith(getUserDto)
+  })
+
+  it('Should return 400 if validation return an error', async () => {
+    const { sut, validationStub } = makeSut()
+    const getUserDto: GetUserDto = {
+      userId: 'foo',
+      document: '11111111',
+      email: 'foo@example',
+      startDate: new Date(),
+      endDate: new Date()
+    }
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new InvalidParamError('email').serializeErrors())
+    const expectedResponse = await sut.handle(getUserDto)
+    expect(expectedResponse.statusCode).toEqual(400)
+    expect(expectedResponse).toEqual(badRequest(new InvalidParamError('email').serializeErrors()))
   })
 
   it('Should return 500 error if getAllUsers throws exception error', async () => {
