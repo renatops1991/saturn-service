dfa41075cab347ed9cb76a251200b13ac10f4b17
Renato Pereira <renatops1991@gmail.com>
2023-01-04 07:56:35 -0300

    feat: ensure call validate method to params validations provided

--- src/main/dtos/user/get-user.dto.ts
+++ src/main/dtos/user/get-user.dto.ts
@@ -1,5 +1,6 @@
 export interface GetUserDto {
   userId: string
+  email?: string
   document?: string
   startDate?: Date
   endDate?: Date
--- src/presentation/controllers/user/get-all-user-controller.ts
+++ src/presentation/controllers/user/get-all-user-controller.ts
@@ -1,14 +1,23 @@
 import { IUser } from '@/domain/protocols/user'
 import { GetUserDto } from '@/main/dtos/user'
-import { serverError, success } from '@/presentation/http-helper'
+import { badRequest, serverError, success } from '@/presentation/http-helper'
 import { IController } from '@/presentation/protocols/controller'
 import { IHttpResponse } from '@/presentation/protocols/http'
+import { IValidation } from '@/presentation/protocols/validation'
 
 export class GetAllUserController implements IController {
-  constructor (private readonly user: IUser) {}
+  constructor (
+    private readonly user: IUser,
+    private readonly validation: IValidation
+  ) { }
 
   async handle (getUserDto: GetUserDto): Promise<IHttpResponse> {
     try {
+      const isError = this.validation.validate(getUserDto)
+      if (isError) {
+        return badRequest(isError)
+      }
+
       const users = await this.user.getAllUsers(getUserDto)
       return success(users)
     } catch (error) {
