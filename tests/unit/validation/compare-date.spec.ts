4d83355a2edba193927b0a14e2c41c42d2dd85a9
Renato Santos <renatops1991@gmail.com>
2023-01-12 00:22:19 -0300

    test: ensure return null if validation on succeeds

--- tests/unit/validation/compare-date.spec.ts
+++ tests/unit/validation/compare-date.spec.ts
@@ -23,4 +23,14 @@ describe('CompareDate', () => {
     expect(expectedError).toEqual(new InvalidDateError('startDate').serializeErrors())
     expect(expectedError.message).toEqual('startDate field cannot greater than endDate field')
   })
+
+  it('Should return null if validation on succeeds', async () => {
+    const { sut } = makeSut()
+    const startDate = '2023-01-12'
+    const endDate = '2023-01-13'
+    const expectedError = sut.validate({
+      startDate, endDate
+    })
+    expect(expectedError).toBeNull()
+  })
 })
