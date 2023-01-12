b9b57e87fce1f7215374ae34771b3e7d706c1e64
Renato Santos <renatops1991@gmail.com>
2023-01-12 00:16:06 -0300

    test: ensure call ValidationComposite with all validations of the getAllUsers validation

---
+++ tests/unit/main/factories/validations/get-all-users-validation-composite-factory.spec.ts
@@ -0,0 +1,24 @@
+import { makeGetAllUsersValidationCompositeFactory } from '@/main/factories/validations/get-all-users-validation-composite-factory'
+import { IValidation } from '@/presentation/protocols/validation'
+import { mockEmailValidator } from '@/tests/unit/presentation/mocks/mocks-user-validation'
+import { CompareDate } from '@/validation/compare-date'
+import { EmailValidation } from '@/validation/email-validation'
+import { ValidationComposite } from '@/validation/validation-composite'
+
+jest.mock('@/validation/validation-composite')
+
+describe('GetAllUsersValidationCompositeFactory', () => {
+  it('Should call ValidationComposite with all validations', () => {
+    makeGetAllUsersValidationCompositeFactory()
+    const validations: IValidation[] = []
+
+    validations.push(
+      new EmailValidation('email', mockEmailValidator())
+    )
+
+    validations.push(
+      new CompareDate('startDate', 'endDate')
+    )
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
